9a7f9f0427ac2d1024b8f1bd7cca37cdb6a9cd4f
Tio-Henry <H3nry.g147@gmail.com>
2026-01-04 14:04:51 -0300

    Work in responsive mobile

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ const screenshots = [
     'assets/images/lavaris/bg.png'
 ];
 
+const text = "Apenas um desenvolvedor solo movido pela paixão de criar mundos e experiências imersivas. Desde 2022, venho transformando ideias em realidade, atuando em todas as etapas da produção — do código ao game design. Sou o criador da Sain&nbsp;Dreams, onde foco no desenvolvimento de projetos como Mindwave: The Sanity e Project World, sempre buscando o equilíbrio entre técnica e atmosfera."
+
 let currentIndex = 0;
 const heroLogoElement = document.querySelector('.hero-logo');
 
