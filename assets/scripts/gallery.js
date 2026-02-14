6259b71cb9dfed9f19095a7b8154313e1dac9a8c
Tio-Henry <H3nry.g147@gmail.com>
2026-02-14 12:14:29 -0300

    Fisined the Mindwave Page, Lang and Mobile Styles

--- assets/scripts/gallery.js
+++ assets/scripts/gallery.js
@@ -1,3 +1,4 @@
+/*
 const photos = [
     "/assets/images/mindwave/1.webp",
     "/assets/images/mindwave/2.webp",
@@ -11,4 +12,27 @@ function change_main_photo(index) {
     if (main_img) {
         main_img.src = photos[index]
     }
+*/
+
+const gallery = document.querySelector(".more_photos")
+const gallery_btn = gallery.querySelectorAll("a")
+
+const main_img = document.querySelector(".main_photo")
+const first_photo = gallery_btn[0].querySelector("img")
+
+function change_main_photo(event) {
+    event.preventDefault();
+
+    const img = event.currentTarget.querySelector("img")
+    main_img.src = img.src
+
+    gallery_btn.forEach((btn) => btn.querySelector("img").classList.remove("active"))
+    img.classList.add("active")
 }
+
+first_photo.classList.add("active")
+
+gallery_btn.forEach((btn) => {
+    btn.addEventListener("click", change_main_photo)
+}
+)
